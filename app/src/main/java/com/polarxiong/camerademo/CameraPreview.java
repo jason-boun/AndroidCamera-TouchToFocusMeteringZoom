c79091ba1a01e232578271f182d5bb01bbc93b5e
jason.jia <jason.jia@dji.com>
2017-10-27 20:40:49 +0800

    format codee

--- app/src/main/java/com/polarxiong/camerademo/CameraPreview.java
+++ app/src/main/java/com/polarxiong/camerademo/CameraPreview.java
@@ -33,16 +33,67 @@ import java.util.Date;
 import java.util.List;
 
 public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+
+    private static final String TAG = "CameraDemo";
+
     public static final int MEDIA_TYPE_IMAGE = 1;
     public static final int MEDIA_TYPE_VIDEO = 2;
-    private static final String TAG = "CameraDemo";
+
     private SurfaceHolder mHolder;
     private Camera mCamera;
     private MediaRecorder mMediaRecorder;
+
     private Uri outputMediaFileUri;
     private String outputMediaFileType;
 
     private float oldDist = 1f;
+
+    public CameraPreview(Context context) {
+        super(context);
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+    }
+
+    public Camera getCameraInstance() {
+        if (mCamera == null) {
+            try {
+                mCamera = Camera.open();
+            } catch (Exception e) {
+                Log.d(TAG, "camera is not available");
+            }
+        }
+        return mCamera;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        getCameraInstance();
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        int rotation = getDisplayOrientation();
+        mCamera.setDisplayOrientation(rotation);
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setRotation(rotation);
+        mCamera.setParameters(parameters);
+        adjustDisplayRatio(rotation);
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mHolder.removeCallback(this);
+        mCamera.setPreviewCallback(null);
+        mCamera.stopPreview();
+        mCamera.release();
+        mCamera = null;
+    }
 
     public void takePicture(final ImageView view) {
         mCamera.takePicture(null, null, new Camera.PictureCallback() {
@@ -92,109 +143,6 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         return mMediaRecorder != null;
     }
 
-    public CameraPreview(Context context) {
-        super(context);
-        mHolder = getHolder();
-        mHolder.addCallback(this);
-    }
-
-    public Camera getCameraInstance() {
-        if (mCamera == null) {
-            try {
-                mCamera = Camera.open();
-            } catch (Exception e) {
-                Log.d(TAG, "camera is not available");
-            }
-        }
-        return mCamera;
-    }
-
-    public void surfaceCreated(SurfaceHolder holder) {
-        getCameraInstance();
-        try {
-            mCamera.setPreviewDisplay(holder);
-            mCamera.startPreview();
-        } catch (IOException e) {
-            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
-        }
-    }
-
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        mHolder.removeCallback(this);
-        mCamera.setPreviewCallback(null);
-        mCamera.stopPreview();
-        mCamera.release();
-        mCamera = null;
-    }
-
-    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        int rotation = getDisplayOrientation();
-        mCamera.setDisplayOrientation(rotation);
-        Camera.Parameters parameters = mCamera.getParameters();
-        parameters.setRotation(rotation);
-        mCamera.setParameters(parameters);
-        adjustDisplayRatio(rotation);
-
-    }
-
-    private void adjustDisplayRatio(int rotation) {
-        ViewGroup parent = ((ViewGroup) getParent());
-        Rect rect = new Rect();
-        parent.getLocalVisibleRect(rect);
-        int width = rect.width();
-        int height = rect.height();
-        Camera.Size previewSize = mCamera.getParameters().getPreviewSize();
-        int previewWidth;
-        int previewHeight;
-        if (rotation == 90 || rotation == 270) {
-            previewWidth = previewSize.height;
-            previewHeight = previewSize.width;
-        } else {
-            previewWidth = previewSize.width;
-            previewHeight = previewSize.height;
-        }
-
-        if (width * previewHeight > height * previewWidth) {
-            final int scaledChildWidth = previewWidth * height / previewHeight;
-
-            layout((width - scaledChildWidth) / 2, 0,
-                    (width + scaledChildWidth) / 2, height);
-        } else {
-            final int scaledChildHeight = previewHeight * width / previewWidth;
-            layout(0, (height - scaledChildHeight) / 2,
-                    width, (height + scaledChildHeight) / 2);
-        }
-    }
-
-    public int getDisplayOrientation() {
-
-        android.hardware.Camera.CameraInfo camInfo =
-                new android.hardware.Camera.CameraInfo();
-        android.hardware.Camera.getCameraInfo(Camera.CameraInfo.CAMERA_FACING_BACK, camInfo);
-
-
-        Display display = ((WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-        int rotation = display.getRotation();
-        int degrees = 0;
-        switch (rotation) {
-            case Surface.ROTATION_0:
-                degrees = 0;
-                break;
-            case Surface.ROTATION_90:
-                degrees = 90;
-                break;
-            case Surface.ROTATION_180:
-                degrees = 180;
-                break;
-            case Surface.ROTATION_270:
-                degrees = 270;
-                break;
-        }
-
-        int result = (camInfo.orientation - degrees + 360) % 360;
-        return result;
-    }
-
     private boolean prepareVideoRecorder() {
 
         mCamera = getCameraInstance();
@@ -241,6 +189,63 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
             mCamera.lock();
         }
     }
+
+    private void adjustDisplayRatio(int rotation) {
+        ViewGroup parent = ((ViewGroup) getParent());
+        Rect rect = new Rect();
+        parent.getLocalVisibleRect(rect);
+        int width = rect.width();
+        int height = rect.height();
+        Camera.Size previewSize = mCamera.getParameters().getPreviewSize();
+        int previewWidth;
+        int previewHeight;
+        if (rotation == 90 || rotation == 270) {
+            previewWidth = previewSize.height;
+            previewHeight = previewSize.width;
+        } else {
+            previewWidth = previewSize.width;
+            previewHeight = previewSize.height;
+        }
+
+        if (width * previewHeight > height * previewWidth) {
+            final int scaledChildWidth = previewWidth * height / previewHeight;
+
+            layout((width - scaledChildWidth) / 2, 0,
+                    (width + scaledChildWidth) / 2, height);
+        } else {
+            final int scaledChildHeight = previewHeight * width / previewWidth;
+            layout(0, (height - scaledChildHeight) / 2,
+                    width, (height + scaledChildHeight) / 2);
+        }
+    }
+
+    public int getDisplayOrientation() {
+
+        android.hardware.Camera.CameraInfo camInfo = new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(Camera.CameraInfo.CAMERA_FACING_BACK, camInfo);
+
+        Display display = ((WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        int rotation = display.getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+
+        int result = (camInfo.orientation - degrees + 360) % 360;
+        return result;
+    }
+
 
     private File getOutputMediaFile(int type) {
         File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
@@ -379,5 +384,13 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
             return min;
         }
         return x;
+    }
+
+    public void releaseData() {
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
     }
 }
--- app/src/main/java/com/polarxiong/camerademo/MainActivity.java
+++ app/src/main/java/com/polarxiong/camerademo/MainActivity.java
@@ -12,51 +12,21 @@ import android.widget.ImageView;
 /**
  * Created by zhantong on 16/4/28.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
     private CameraPreview mPreview;
+    private ImageView mediaPreview;
+    private Button buttonCapturePhoto;
+    private Button buttonCaptureVideo;
+    private Button buttonSettings;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initCamera();
-        Button buttonSettings = (Button) findViewById(R.id.button_settings);
-        buttonSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getFragmentManager().beginTransaction().replace(R.id.camera_preview, new SettingsFragment()).addToBackStack(null).commit();
-            }
-        });
-        final ImageView mediaPreview = (ImageView) findViewById(R.id.media_preview);
-        final Button buttonCapturePhoto = (Button) findViewById(R.id.button_capture_photo);
-        buttonCapturePhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mPreview.takePicture(mediaPreview);
-            }
-        });
-        final Button buttonCaptureVideo = (Button) findViewById(R.id.button_capture_video);
-        buttonCaptureVideo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mPreview.isRecording()) {
-                    mPreview.stopRecording(mediaPreview);
-                    buttonCaptureVideo.setText("录像");
-                } else {
-                    if (mPreview.startRecording()) {
-                        buttonCaptureVideo.setText("停止");
-                    }
-                }
-            }
-        });
-        mediaPreview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ShowPhotoVideo.class);
-                intent.setDataAndType(mPreview.getOutputMediaFileUri(), mPreview.getOutputMediaFileType());
-                startActivityForResult(intent, 0);
-            }
-        });
+        initView();
+
     }
 
     private void initCamera() {
@@ -70,6 +40,21 @@ public class MainActivity extends Activity {
         SettingsFragment.init(PreferenceManager.getDefaultSharedPreferences(this));
     }
 
+    private void initView() {
+        buttonSettings = (Button) findViewById(R.id.button_settings);
+        buttonSettings.setOnClickListener(this);
+
+        buttonCapturePhoto = (Button) findViewById(R.id.button_capture_photo);
+        buttonCapturePhoto.setOnClickListener(this);
+
+        buttonCaptureVideo = (Button) findViewById(R.id.button_capture_video);
+        buttonCaptureVideo.setOnClickListener(this);
+
+        mediaPreview = (ImageView) findViewById(R.id.media_preview);
+        mediaPreview.setOnClickListener(this);
+    }
+
+
     public void onPause() {
         super.onPause();
         mPreview = null;
@@ -80,5 +65,41 @@ public class MainActivity extends Activity {
         if (mPreview == null) {
             initCamera();
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_settings:
+                getFragmentManager().beginTransaction().replace(R.id.camera_preview, new SettingsFragment()).addToBackStack(null).commit();
+                break;
+            case R.id.button_capture_photo:
+                mPreview.takePicture(mediaPreview);
+                break;
+            case R.id.button_capture_video:
+                if (mPreview.isRecording()) {
+                    mPreview.stopRecording(mediaPreview);
+                    buttonCaptureVideo.setText("录像");
+                } else {
+                    if (mPreview.startRecording()) {
+                        buttonCaptureVideo.setText("停止");
+                    }
+                }
+                break;
+            case R.id.media_preview:
+                Intent intent = new Intent(MainActivity.this, ShowPhotoVideo.class);
+                intent.setDataAndType(mPreview.getOutputMediaFileUri(), mPreview.getOutputMediaFileType());
+                startActivityForResult(intent, 0);
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mPreview != null) {
+            mPreview.releaseData();
+            mPreview = null;
+        }
+        super.onDestroy();
     }
 }
